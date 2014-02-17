f21fe90d87d14c6b4add81b0af4fab5ed4744f1e
anastasia143 <semenova.anastasia143@yandex.ru>
2014-02-17 18:51:27 +0400

    Text corrections

--- factorial.hs
+++ factorial.hs
@@ -1,9 +1,9 @@
 factorial :: Integer -> Integer
 factorial 0 = 1
-factorial n | n < 0 = error "Argument shold be not negative" | otherwise = n * factorial(n - 1)
+factorial n | n < 0 = error "Argument should be not negative" | otherwise = n * factorial(n - 1)
 
 main = do
-	putStrLn "Enter n: "
+	putStrLn "Enter number for factorial calculation: "
 	n <- readLn
 	putStrLn "n! = "
 	print (factorial n)
--- fibonachi.hs
+++ fibonachi.hs
@@ -1,7 +1,7 @@
 fib :: Integer -> Integer
 fib 1 = 1
 fib 2 = 1
-fib n | n > 0 = fib(n - 1) + fib(n - 2) | otherwise = error "Argument shold be positive"
+fib n | n > 0 = fib(n - 1) + fib(n - 2) | otherwise = error "Argument should be positive"
 
 main = do
 	putStrLn "Enter serial number of desired fibonachi number: "
