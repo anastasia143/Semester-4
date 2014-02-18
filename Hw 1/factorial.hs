0444865bfda49ad3719e8d3811e1db743a7760bd
anastasia143 <semenova.anastasia143@yandex.ru>
2014-02-18 17:36:38 +0400

    Replacement, stile-guide

--- Hw 1/factorial.hs
+++ Hw 1/factorial.hs
@@ -1,6 +1,7 @@
 factorial :: Integer -> Integer
 factorial 0 = 1
-factorial n | n < 0 = error "Argument should be not negative" | otherwise = n * factorial(n - 1)
+factorial n | n < 0 = error "Argument should be not negative" 
+            | otherwise = n * factorial(n - 1)
 
 main = do
 	putStrLn "Enter number for factorial calculation: "
--- Hw 1/fibonachi.hs
+++ Hw 1/fibonachi.hs
@@ -1,7 +1,8 @@
 fib :: Integer -> Integer
 fib 1 = 1
 fib 2 = 1
-fib n | n > 0 = fib(n - 1) + fib(n - 2) | otherwise = error "Argument should be positive"
+fib n | n > 0 = fib(n - 1) + fib(n - 2) 
+      | otherwise = error "Argument should be positive"
 
 main = do
 	putStrLn "Enter serial number of desired fibonachi number: "
