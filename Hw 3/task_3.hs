6c3d9ba55d8470afac2ecc5949d4e68470d27b1e
anastasia143 <semenova.anastasia143@yandex.ru>
2014-03-06 20:09:39 +0400

    Hw 3, task 3

--- Hw 3/task_3.hs
+++ Hw 3/task_3.hs
@@ -1,15 +1,17 @@
 import Data.Char
+figures :: Int -> [Int]
+figures n = map (\ch -> ord ch - ord '0') $ show n
 
-numbers :: Int -> [Int]
-numbers n = map (\ch -> ord ch - ord '0') $ show n
+next :: Int -> Int
+next 1 = 7
+next 7 = 9
+next 9 = 11
+next x | lastCh == 9 = 1 + next tenDiv * 10
+       | otherwise = tenDiv * 10 + mod (next lastCh) 10
+     where lastCh = head $ reverse $ figures x
+           tenDiv = div x 10
 
-check :: [Int] -> Bool
-check [] = True
-check (n:ns) | (n == 1 || n == 7 || n == 9) = check(ns)
-             | otherwise = False
-             
-list :: [Int]
-list = [x | x <- [1..], check $ numbers x]
+list = 1: map next list
 
 main = do
-    putStrLn(show $ take 13 list)
+     putStrLn(show $ take 15 list)
