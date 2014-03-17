042fa2bd8ff076caea4ef6a5e134f1fe2a218304
anastasia143 <semenova.anastasia143@yandex.ru>
2014-03-17 22:51:56 +0400

    mplus instead of ++

--- Hw 5/task_3.hs
+++ Hw 5/task_3.hs
@@ -2,7 +2,7 @@ import Control.Monad
 
 list123 :: Int -> [[Int]]
 list123 1 = [[1],[2],[3]]
-list123 n = liftM2 (++) [[1], [2], [3]] (list123 (n - 1))
+list123 n = liftM2 (mplus) [[1], [2], [3]] (list123 (n - 1))
 
 main = do
     putStrLn(show $ list123 2)
