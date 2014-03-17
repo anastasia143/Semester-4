c6b865c0045febb3a841e5539b52e4b3cda8ad9e
anastasia143 <semenova.anastasia143@yandex.ru>
2014-03-17 20:27:24 +0400

    Hw 5, task 3

---
+++ Hw 5/task_3.hs
@@ -0,0 +1,9 @@
+import Control.Monad
+
+list123 :: Int -> [[Int]]
+list123 1 = [[1],[2],[3]]
+list123 n = liftM2 (++) [[1], [2], [3]] (list123 (n - 1))
+
+main = do
+    putStrLn(show $ list123 2)
+    putStrLn(show $ list123 3)
